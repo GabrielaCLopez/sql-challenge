e69eb803bafe9031b751740949c833537014f63b
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-02-15 20:13:38 -0800

    redid to include not null

--- EmployeeSQL/employee.sql
+++ EmployeeSQL/employee.sql
@@ -7,45 +7,48 @@ DROP TABLE IF EXISTS salaries
 DROP TABLE IF EXISTS titles
 
 CREATE TABLE departments(
-  	dept_no VARCHAR,
-	dept_name VARCHAR,
+  	dept_no VARCHAR NOT NULL, 
+	dept_name VARCHAR NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE titles(
-  	titleID VARCHAR,
-	title VARCHAR,
+  	titleID VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
 	PRIMARY KEY (titleID)
 );
 
 CREATE TABLE employees(
-  	emp_no INTEGER,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex CHAR,
-	hire_date DATE,
+  	emp_no INTEGER NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex CHAR NOT NULL,
+	hire_date DATE NOT NULL,
   	PRIMARY KEY(emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(titleID)
 );
 
 CREATE TABLE dept_emp(
-  	emp_no INTEGER,
-	dept_no VARCHAR,
+  	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR NOT NULL,
   	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-  	dept_no VARCHAR,
-	emp_no INTEGER,
+  	dept_no VARCHAR NOT NULL,
+	emp_no INTEGER NOT NULL,
   	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries(
-  	emp_no INTEGER,
-	salary INTEGER,
+  	emp_no INTEGER NOT NULL,
+	salary INTEGER NOT NULL,
   	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
+SELECT * from salaries
+
